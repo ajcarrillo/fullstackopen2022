5fe7288a86b674a0952c6321125ef4a980e5b713
Andrés Carrillo Herrera <andresjch2804@gmail.com>
2022-06-22 01:00:42 -0500

    Part 2 excercises 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,24 @@
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  const fetchPersons = () => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(fetchPersons, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -29,6 +34,8 @@ function App() {
     if (event.target.value !== '') {
       const filteredPersonsByName = filteredPersons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
       setPersons(filteredPersonsByName)
+    } else {
+      fetchPersons()
     }
   }
 
